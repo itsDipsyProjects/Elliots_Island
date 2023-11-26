0e0c5d72889aed7880ff228e13d9d03f2b203198
itsDipsy <elliot.collinszcv@gmail.com>
2023-11-27 00:51:28 +0100

    Update test.js

--- portfolio_page/test.js
+++ portfolio_page/test.js
@@ -1,12 +1,12 @@
 let intro_text = document.querySelector("#intro_text");
-let counter_down = 0;
+let counter_down = 500;
 
 function test() {
   window.requestAnimationFrame(test);
   console.log(counter_down)
   if(counter_down !== -2000 && counter_down <= 0){
     // Adjust the animation speed by changing the decrement value
-    counter_down = counter_down - 2; // or any other value based on your preference
+    counter_down = counter_down - 1.5; // or any other value based on your preference
   
     // Use units for the translate value (e.g., px)
     intro_text.style.transform = `translateX(${counter_down}px)`;
@@ -18,7 +18,7 @@ function test() {
   }
 
   if(counter_down <= 2500 && counter_down !== 0){
-    counter_down = counter_down - 2; // or any other value based on your preference
+    counter_down = counter_down - 1.5; // or any other value based on your preference
   
     // Use units for the translate value (e.g., px)
     intro_text.style.transform = `translateX(${counter_down}px)`;
