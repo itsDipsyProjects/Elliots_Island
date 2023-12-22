fa2f6f9bb17736b0beaa8c890c433a99a2413ca2
itsDipsy <elliot.collinszcv@gmail.com>
2023-12-22 23:17:47 +0100

    uppdate

--- portfolio_page/utils/the_check.js
+++ portfolio_page/utils/the_check.js
@@ -1,5 +1,10 @@
 if(localStorage.getItem("clicked_on_my_work") !== undefined && localStorage.getItem("clicked_on_my_work") === "true"){
     localStorage.removeItem("clicked_on_my_work")
+     /*
+        TODO: 
+        Här måste jag kolla olika skärm stolekar med window.innerWidth för att förstå vart den ska
+        ska scrolla till i pixlar för det ändras när man schyssterar skärmen.
+    */
     window.scrollTo({
         top: 1530,
         behavior: "smooth",
@@ -8,6 +13,11 @@ if(localStorage.getItem("clicked_on_my_work") !== undefined && localStorage.getI
 
 if(localStorage.getItem("clicked_on_about_me") !== undefined && localStorage.getItem("clicked_on_about_me") === "true"){
     localStorage.removeItem("clicked_on_about_me")
+     /*
+        TODO: 
+        Här måste jag kolla olika skärm stolekar med window.innerWidth för att förstå vart den ska
+        ska scrolla till i pixlar för det ändras när man schyssterar skärmen.
+    */
     window.scrollTo({
         top: 2750,
         behavior: "smooth",
@@ -17,6 +27,11 @@ if(localStorage.getItem("clicked_on_about_me") !== undefined && localStorage.get
 
 if(localStorage.getItem("clicked_on_contact") !== undefined && localStorage.getItem("clicked_on_contact") === "true"){
     localStorage.removeItem("clicked_on_contact")
+     /*
+        TODO: 
+        Här måste jag kolla olika skärm stolekar med window.innerWidth för att förstå vart den ska
+        ska scrolla till i pixlar för det ändras när man schyssterar skärmen.
+    */
     window.scrollTo({
         top: 5050,
         behavior: "smooth",
