df4c3099430cbdbe2c120cd3030353488cc2869b
itsDipz <131451520+itsDipz@users.noreply.github.com>
2023-12-23 16:35:21 +0100

    uppdate

--- portfolio_page/utils/the_check.js
+++ portfolio_page/utils/the_check.js
@@ -1,59 +1,24 @@
 if(localStorage.getItem("clicked_on_my_work") !== undefined && localStorage.getItem("clicked_on_my_work") === "true"){
     localStorage.removeItem("clicked_on_my_work")
-    
-        if(window.innerWidth >= 1050){
-            window.scrollTo({
-                top: 1450,
-                behavior: "smooth",
-            });
-        }
-        if(window.innerWidth <= 1050){
-            window.scrollTo({
-                top: 1129,
-                behavior: "smooth",
-            });
-        }
+    document.querySelector("#second_section").scrollIntoView({
+        behavior:"smooth"
+    });
 }
 
 if(localStorage.getItem("clicked_on_about_me") !== undefined && localStorage.getItem("clicked_on_about_me") === "true"){
     localStorage.removeItem("clicked_on_about_me")
-   
-        if(window.innerWidth >= 1050){
-            window.scrollTo({
-                top: 1450,
-                behavior: "smooth",
-            });
-        }
-        if(window.innerWidth <= 1050){
-            window.scrollTo({
-                top: 1129,
-                behavior: "smooth",
-            });
-        }
-        if(window.innerWidth >= 1650){
-            window.scrollTo({
-                top: 1050,
-                behavior: "smooth",
-            });
-        }
+    document.querySelector("#third_section").scrollIntoView({
+        behavior:"smooth"
+    });
+       
 }
 
 
 if(localStorage.getItem("clicked_on_contact") !== undefined && localStorage.getItem("clicked_on_contact") === "true"){
     localStorage.removeItem("clicked_on_contact")
-
-        if(window.innerWidth >= 1050){
-            window.scrollTo({
-                top: 4550,
-                behavior: "smooth",
-            });
-        }
-
-        if(window.innerWidth <= 1050){
-            window.scrollTo({
-                top: 6550,
-                behavior: "smooth",
-            });
-        }
+    document.querySelector("#fourth_section").scrollIntoView({
+        behavior:"smooth"
+    });
+        
 }
 
